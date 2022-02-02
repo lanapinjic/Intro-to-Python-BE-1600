1be8b7d3b23b44768f5c1e2778aa68350bccc794
Lana Pinjic <92830734+lanapinjic@users.noreply.github.com>
2022-02-02 14:55:20 -0500

    Delete Lab 01.py

--- Lab 01.py
+++
@@ -1,9 +0,0 @@
-def d(a):
-    y=a
-    print("Circle Radius = ", y)
-    print("Diameter = ", 2*y)
-    print("Circumference = ", 3.14*y*2)
-    print("Area = ", 3.14*(y**2))
-
-b=float(input("Enter the radius of a circle "))
-d(b)
