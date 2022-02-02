2c55e618244cf1e919f9032f8fe5a26a3bed8e5b
Lana Pinjic <92830734+lanapinjic@users.noreply.github.com>
2022-02-02 14:57:06 -0500

    Delete Lab 10.py

--- Lab 10.py
+++
@@ -1,9 +0,0 @@
-def func(d1, d2):
-    new_d = {i : d1[i] for i in d1 if i in d2 and d1[i]==d2[i]}
-    return new_d
-d1 = {'Janet':87,'Logan':62,'Whitaker':46,'Alyssa':100,'Stef':80,'Jeff':88,'Kim':52,'Sylvia':95}
-d2 = {'Logan':62,'Kim':52,'Whitaker':52,'Jeff':88,'Stef':80,'Brian':60,'Lisa':83,'Sylvia':87}
-
-new_d = func(d1,d2)
-print(new_d)
-
